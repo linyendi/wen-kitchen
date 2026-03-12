275f293d20b87cc7fc5871cca31ebe9273935ce8
linyendi <tree30631@gmail.com>
2026-03-12 15:51:12 +0800

    Update MenuController.java

--- backend/src/main/java/com/wen/MenuController.java
+++ backend/src/main/java/com/wen/MenuController.java
@@ -1,6 +1,6 @@
 package com.wen;
 
-import org.springframework.web.bind.annotation.CrossOrigin; // 記得要 import
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,26 +17,32 @@ public class MenuController {
 
         // 1. 菜單資料
         List<Map<String, Object>> menu = new ArrayList<>();
+
+        // --- 雞蛋燒系列 (新增 options 欄位) ---
+        List<Map<String, Object>> eggBurnItems = new ArrayList<>();
+        eggBurnItems.add(createItem("肉鬆起司蛋", 45, "人氣推薦", Arrays.asList("全熟蛋")));
+        eggBurnItems.add(createItem("只有肉鬆蛋", 40, "", Arrays.asList("全熟蛋")));
+        eggBurnItems.add(createItem("起司蛋燒", 40, "", Arrays.asList("全熟蛋")));
         
-        menu.add(createCategory("雞蛋燒", Arrays.asList(
-            createItem("肉鬆起司蛋", 45, "人氣推薦 / 全熟蛋"),
-            createItem("只有肉鬆蛋", 40, "全熟蛋"),
-            createItem("起司蛋燒", 40, "全熟蛋")
-        )));
+        menu.add(createCategory("雞蛋燒", eggBurnItems));
 
-        menu.add(createCategory("雞蛋糕", Arrays.asList(
-            createItem("古早味雞蛋糕", 20, "無餡料"),
-            createItem("香濃奶酥", 35, ""),
-            createItem("醇黑巧OREO", 35, ""),
-            createItem("黑糖麻糬", 30, ""),
-            createItem("原味卡士達", 30, ""),
-            createItem("今日私房口味", null, "請看社群或現場問老闆")
-        )));
+        // --- 雞蛋糕系列 ---
+        List<Map<String, Object>> eggCakeItems = new ArrayList<>();
+        eggCakeItems.add(createItem("古早味雞蛋糕", 20, "無餡料", null));
+        eggCakeItems.add(createItem("香濃奶酥", 35, "", null));
+        eggCakeItems.add(createItem("醇黑巧OREO", 35, "", null));
+        eggCakeItems.add(createItem("黑糖麻糬", 30, "", null));
+        eggCakeItems.add(createItem("原味卡士達", 30, "", null));
+        eggCakeItems.add(createItem("今日私房口味", null, "請看社群或現場問老闆", null));
 
-        menu.add(createCategory("呷涼的", Arrays.asList(
-            createItem("甘甜冷泡茶", 25, ""),
-            createItem("蜜香紅茶", 25, "")
-        )));
+        menu.add(createCategory("雞蛋糕", eggCakeItems));
+
+        // --- 呷涼的系列 ---
+        List<Map<String, Object>> drinksItems = new ArrayList<>();
+        drinksItems.add(createItem("甘甜冷泡茶", 25, "", null));
+        drinksItems.add(createItem("蜜香紅茶", 25, "", null));
+
+        menu.add(createCategory("呷涼的", drinksItems));
 
         // 2. 商家資訊
         Map<String, String> shopInfo = new HashMap<>();
@@ -57,11 +63,13 @@ public class MenuController {
         return category;
     }
 
-    private Map<String, Object> createItem(String name, Integer price, String note) {
+    // 更新 createItem 方法，加入 options
+    private Map<String, Object> createItem(String name, Integer price, String note, List<String> options) {
         Map<String, Object> item = new HashMap<>();
         item.put("name", name);
         item.put("price", price);
         item.put("note", note);
+        item.put("options", options); // 新增這行
         return item;
     }
 }
